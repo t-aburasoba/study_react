de0330d4df63a716c6e02f46aef2b6630b6d3638
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2021-12-01 16:12:21 +0900

    add index page

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,11 +1,22 @@
 import Layout from "../components/Layout";
+import { getAllPostsData } from "../lib/posts";
+import Post from "../components/Post";
 
-const Blog = () => {
+const Blog = ({ posts }) => {
     return (
         <Layout title="Blog">
-            Blog Page
+            <ul className="mt-10">
+                {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+            </ul>
         </Layout>
     )
 };
 
 export default Blog;
+
+export async function getStaticProps(){
+    const posts = await getAllPostsData();
+    return {
+        props: { posts }
+    }
+}
